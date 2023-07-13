8598153ecf0b653c7c13d1685e632352129b75e5
publdaze <publdaze@naver.com>
2023-07-13 13:48:19 +0900

    fix: 객체 데이터 특정 키만 받아오도록 변경

--- src/floors/floors.controller.ts
+++ src/floors/floors.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateFloorDto } from './dtos/create-floor.dto';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { FloorsService } from './floors.service';
 
-@Controller('floors')
+@Controller()
 export class FloorsController {
   constructor(private readonly floorsService: FloorsService) {}
 
-  @Post()
-  createFloor(@Body() floorData: CreateFloorDto) {
-    return this.floorsService.createFloor(floorData);
+  @Post('/buildings/:buildingNumber/floors')
+  createFloor(
+    @Param('buildingNumber') buildingNumber: number,
+    @Body('floorNumber') floorNumber: number,
+  ) {
+    return this.floorsService.createFloor({ buildingNumber, floorNumber });
   }
 }
