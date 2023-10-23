7ccd783b1e1a08a68df5c297768db08633a60a5b
publdaze <publdaze@naver.com>
2023-10-23 17:57:16 +0900

    feat : 알림 수신 여부 API 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { ApiOperation } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/passport/role.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +14,12 @@ export class UsersController {
   @ApiOperation({ summary: '회원가입' })
   async create(@Body() userData: CreateUserDto): Promise<User> {
     return await this.userService.create(userData);
+  }
+
+  @Put('/notification')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  async setNotificationEnable(@Body() req) {
+    console.log(req);
+    return await this.userService.setNotificationEnable(req.body);
   }
 }
